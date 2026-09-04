f51064df0c49353be9707fc0e3d77fa8ad41c161
박상욱 <choryunjp@gmail.com>
2026-09-04 17:45:31 +0900

    Generate Window

---
+++ SPSoftRenderer/main.cpp
@@ -0,0 +1,98 @@
+#include <windows.h>
+
+// 메시지 처리 함수
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (message)
+	{
+	case WM_DESTROY:
+		// Signal that the app should quit
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	// 윈도우 클래스
+	WCHAR WindowClass[] = L"SPSoftRenderer";
+
+	// 윈도우 타이틀바
+	WCHAR Title[] = L"SPSoftRenderer";
+
+	// 메시지 처리 함수 WndProc의 함수 포인터를 WindowClass 구조체에 삽입
+	WNDCLASSW wndclass = { 0, WndProc, 0, 0, 0, 0, 0, 0, 0, WindowClass };
+
+	// 윈도우 클래스 등록
+	if (RegisterClassW(&wndclass) == 0)
+	{
+		return -1;
+	}
+
+	// 1024 x 1024 size 윈도우 생성
+	HWND hWnd = CreateWindowExW(0, WindowClass, Title, WS_POPUP | WS_VISIBLE | WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 1024, 1024,
+		nullptr, nullptr, hInstance, nullptr);
+
+	if (hWnd == nullptr)
+	{
+		return -1;
+	}
+
+	bool bIsExit = false;
+		
+	// FPS 제한
+	const int targetFPS = 60;
+	const double targetFrameTime = 1000.0 / targetFPS; // 한 프레임의 목표 시간 (밀리초 단위)
+
+	// 고성능 타이머 초기화
+	LARGE_INTEGER frequency;
+	QueryPerformanceFrequency(&frequency);
+
+	LARGE_INTEGER startTime, endTime;
+	double elapsedTime = 0.0;
+
+	// Main Loop (Quit Message가 들어오기 전까지 아래 Loop를 무한히 실행하게 됨)
+	while (bIsExit == false)
+	{
+		MSG msg;
+
+		// 루프 시작 시간 기록
+		QueryPerformanceCounter(&startTime);
+
+		// 처리할 메시지가 더 이상 없을때 까지 수행
+		while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			// 키 입력 메시지를 번역
+			TranslateMessage(&msg);
+
+			// 메시지를 적절한 윈도우 프로시저에 전달, 메시지가 위에서 등록한 WndProc 으로 전달됨
+			DispatchMessage(&msg);
+
+			if (msg.message == WM_QUIT)
+			{
+				bIsExit = true;
+				break;
+			}
+		}
+
+		do
+		{
+			Sleep(0);
+
+			// 루프 종료 시간 기록
+			QueryPerformanceCounter(&endTime);
+
+			// 한 프레임이 소요된 시간 계산 (밀리초 단위로 변환)
+			elapsedTime = (endTime.QuadPart - startTime.QuadPart) * 1000.0 / frequency.QuadPart;
+
+		} while (elapsedTime < targetFrameTime);
+	}
+
+	return 0;
+}
